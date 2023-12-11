9affd44e4333b8fd01a85d11fd1ebcc311878fda
FedericoM120 <monteverdi.102@gmail.com>
2023-12-11 00:34:42 -0500

    changing logic in boolean while statement to check if matchingLetterIndex index has been chosen

--- src/App.java
+++ src/App.java
@@ -1,9 +1,46 @@
 import java.util.Scanner;
+import java.util.Random;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] actualChoices = new String[]{"C", "D", "A", "B", "B", "D", "A", "C"};
+        //String[] actualChoices = new String[]{"C", "D", "A", "B", "B", "D", "A", "C"};
+
+        Random r = new Random();
+        char[] actualChoices = new char[8];
+
+        int[] indexesChosen = new int[]{10,10,10,10,10,10,10,10};
+
+        for (int l = 0; l < actualChoices.length; l++){
+            char memoryGameValue = (char)(r.nextInt(26) + 'A');
+
+            //actualChoices[l] = memoryGameValue;
+            for (int m = l; m < indexesChosen.length; m++){
+                if (indexesChosen[m] != 10) {
+                    l++;
+                } else {
+                    break;
+                }
+            }
+            actualChoices[l] = memoryGameValue;
+            indexesChosen[l] = l;
+            int matchingLetterIndex = r.nextInt(8 - l) + l;
+            boolean indexFound = false;
+            while (indexFound == false) {
+                // matchingLetterIndex is in the array of indexesChosen, then keep generating a number until its not in the array
+                for (int j = l; j < indexesChosen.length; j++){
+                    if (matchingLetterIndex == indexesChosen[j]) {
+                        matchingLetterIndex = r.nextInt(8);
+                        j = 0;
+                    } else {
+                        actualChoices[matchingLetterIndex] = memoryGameValue;
+                        indexesChosen[matchingLetterIndex] = matchingLetterIndex;
+                        indexFound = true;
+                        break;
+                    }
+                }
+            }
+        }
 
         String[] appearance = new String[]{"null", "null", "null", "null", "null", "null", "null", "null"};
 
@@ -51,8 +88,8 @@ public class App {
 
             //if choices match then they need appear permanently
             if (actualChoices[firstIndex] == actualChoices[secondIndex]) {
-                appearance[firstIndex] = actualChoices[firstIndex];
-                appearance[secondIndex] = actualChoices[secondIndex];
+                appearance[firstIndex] = String.valueOf(actualChoices[firstIndex]);
+                appearance[secondIndex] = String.valueOf(actualChoices[secondIndex]);
             } else if (actualChoices[firstIndex] != actualChoices[secondIndex]) {
                 appearance[firstIndex] = " ";
                 appearance[secondIndex] = " ";
